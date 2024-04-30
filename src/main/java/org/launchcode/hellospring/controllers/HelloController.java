43b25332e90094f0705f5571dd9fbaaa7efb505e
Aswini Raghul <aswiniraghul@gmail.com>
2024-04-30 14:12:33 -0500

    updated thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,94 +1,55 @@
 package org.launchcode.hellospring.controllers;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
-    //handle request at path http://localhost:8080/hello
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
-
-    //lives/hello/goodbye
-// handle request at path //http://localhost:8080/hello/goodbye
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //lives /hello/hello
-    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     public String helloWithQueryParam(@RequestParam String name, Model model) {
         String greeting = "Hello, " + name + "!";
-        model.addText("greeting");
+        model.addAttribute("greeting", greeting);
         return "hello";
     }
 
-
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-
-   // handles requests for http://localhost:8080/hello/form
+    // /hello/form
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
-    //Comments
-    @RequestMapping(value = "hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
 
-        return createMessage(name, language);
-
-        // For a bonus mission, students can change this response text to look nicer.
-        // This is subjective, but students should be modifying the HTML of the response string.
     }
 
-    public static String createMessage(String n, String l) {
-        String greeting = "";
-
-        if (l.equals("english")) {
-            greeting = "Hello";
-        } else if (l.equals("french")) {
-            greeting = "Bonjour";
-        } else if (l.equals("italian")) {
-            greeting = "Bonjourno";
-        } else if (l.equals("spanish")) {
-            greeting = "Hola";
-        } else if (l.equals("german")) {
-            greeting = "Hallo";
-        }
-
-        return greeting + " " + n;
-    }
 }
